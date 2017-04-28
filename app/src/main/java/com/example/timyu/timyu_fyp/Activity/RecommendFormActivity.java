5fcf2488b5cef1975ffc30ab91c611c69ba1e3f8
timyu12390 <timyu12390@gmail.com>
2017-04-28 21:10:50 +0800

    timyu

--- app/src/main/java/com/example/timyu/timyu_fyp/Activity/RecommendFormActivity.java
+++ app/src/main/java/com/example/timyu/timyu_fyp/Activity/RecommendFormActivity.java
@@ -1,34 +1,48 @@
 package com.example.timyu.timyu_fyp.Activity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.example.timyu.timyu_fyp.R;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import ernestoyaquello.com.verticalstepperform.VerticalStepperFormLayout;
 import ernestoyaquello.com.verticalstepperform.*;
 import ernestoyaquello.com.verticalstepperform.interfaces.VerticalStepperForm;
 
-public class RecommendFormActivity extends AppCompatActivity implements VerticalStepperForm {
+public class RecommendFormActivity extends AppCompatActivity implements VerticalStepperForm, PlaceSelectionListener {
     private VerticalStepperFormLayout verticalStepperForm;
     EditText name;
+    String TAG = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend_form);
 
-        String[] mySteps = {"Name", "Email", "Phone Number"};
+        String[] mySteps = {"Start Point", "Must Go", "Phone Number"};
         int colorPrimary = ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary);
         int colorPrimaryDark = ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark);
 
         // Finding the view
         verticalStepperForm = (VerticalStepperFormLayout) findViewById(R.id.vertical_stepper_form);
+
+
 
         // Setting up and initializing the form
         VerticalStepperFormLayout.Builder.newInstance(verticalStepperForm, mySteps, this, this)
@@ -68,7 +82,12 @@ public class RecommendFormActivity extends AppCompatActivity implements Vertical
         // In this case we generate the view by inflating a XML file
         LayoutInflater inflater = LayoutInflater.from(getBaseContext());
         LinearLayout emailLayoutContent = (LinearLayout) inflater.inflate(R.layout.email_step_layout, null, false);
-        EditText email = (EditText) emailLayoutContent.findViewById(R.id.email);
+
+        PlaceAutocompleteFragment startPointSelect = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        //startPointSelect.setOnPlaceSelectedListener(this);
+        startPointSelect.setOnPlaceSelectedListener(this);
+
 
         return emailLayoutContent;
     }
@@ -100,21 +119,66 @@ public class RecommendFormActivity extends AppCompatActivity implements Vertical
     }
 
     private void checkName() {
-        if(name.length() >= 3 && name.length() <= 40) {
-            verticalStepperForm.setActiveStepAsCompleted();
-        } else {
-            // This error message is optional (use null if you don't want to display an error message)
-            String errorMessage = "The name must have between 3 and 40 characters";
-            verticalStepperForm.setActiveStepAsUncompleted(errorMessage);
-        }
+
+        //Continue button
+        verticalStepperForm.setActiveStepAsCompleted();
+
     }
 
     private void checkEmail() {
+
+        //Continue button
+        verticalStepperForm.setActiveStepAsCompleted();
 
     }
 
     @Override
     public void sendData() {
+
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+	    super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    //@Override
+    public void onPlaceSelected(Place place) {
+        //placeItem.setPlaceName(place);
+
+        //Log.i(TAG, "Place Selected: " + place.getName());
+
+        // Format the returned place's details and display them in the TextView.
+        //txtPlaceName.setText(place.getName());
+
+        /*
+        placeId = place.getId();
+        placeName = (String) place.getName();
+        placeInfo = String.valueOf(place.getWebsiteUri());
+        placeAddress = (String) place.getAddress();
+        placeLat = place.getLatLng().latitude;
+        placeLng = place.getLatLng().longitude;
+        */
+        /*
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = geocoder.getFromLocation(placeLat, placeLng, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        placeCountry = addresses.get(0).getCountryName();
+        */
+
+    }
+
+    @Override
+    public void onError(Status status) {
 
     }
 }
